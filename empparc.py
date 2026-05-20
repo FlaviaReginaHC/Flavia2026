c35fa94ffdbf4ec7b22957adcd14a79dc5906c64
FlaviaReginaHC <flavia.regina@academico.ifpb.edu.br>
2026-05-20 11:37:29 -0300

    Update empparc.py

--- empparc.py
+++ empparc.py
@@ -1,1 +1,24 @@
+import streamlit as st
 
+st.title("Empresas Parceiras")
+
+col1, col2, col3 = st.columns([1, 2, 3])
+
+with col1:
+  st.image("spacex.jpg", width=300)
+  st.write("Space Exploration Technologies Corp., cujo nome comercial é SpaceX, é uma fabricante estadunidense de sistemas aeroespaciais, transporte espacial e comunicações com sede em Boca Chica, perto de Brownsville, no Texas.")
+  st.link_button("Acessar Site", "https://www.spacex.com/")
+  
+with col2:
+  st.image("apple.jpg", width=300)
+  st.write("A Apple é uma multinacional americana. Famosa por revolucionar a tecnologia pessoal com o iPhone, Mac e iPad, ela destaca-se pelo design premium e a forte integração entre seus produtos.")
+  st.link_button("Acessar Site", "https://www.apple.com/br/")
+
+with col3:
+  st.image("netflix.jpg", width=300)
+  st.write("A Netflix é o principal serviço de streaming por assinatura do mundo. A plataforma permite assistir a um vasto catálogo de séries, filmes e documentários em TVs, celulares e computadores.")
+  st.link_button("Acessar Site", "https://www.netflix.com/br/")
+
+st.write("")
+
+st.markdown("<br>", unsafe_allow_html=True)

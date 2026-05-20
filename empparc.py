f5f86830b5e87ae28eb4fad358744fba24986993
FlaviaReginaHC <flavia.regina@academico.ifpb.edu.br>
2026-05-20 12:10:17 -0300

    Update empparc.py

--- empparc.py
+++ empparc.py
@@ -8,19 +8,19 @@ col1, col2, col3 = st.columns(3)
 with col1:
   st.image("spacex.jpg")
   st.title("SpaceX")
-  st.write("SpaceX é uma fabricante estadunidense de sistemas aeroespaciais, transporte espacial e comunicações com sede em Boca Chica, perto de Brownsville, no Texas.")
+  st.write("Fabricante estadunidense de sistemas aeroespaciais, transporte espacial e comunicaçõe.")
   st.link_button("Acessar Site", "https://www.spacex.com/")
   
 with col2:
   st.image("apple.jpg")
   st.title("Apple")
-  st.write("A Apple é uma multinacional americana. Famosa por revolucionar a tecnologia pessoal com o iPhone, Mac e iPad, ela destaca-se pelo design premium e a forte integração entre seus produtos.")
+  st.write("Multinacional americana. Famosa por revolucionar a tecnologia pessoal com o iPhone, Mac e iPad.")
   st.link_button("Acessar Site", "https://www.apple.com/br/")
   
 with col3:
   st.image("netflix.jpg")
   st.title("Netflix")
-  st.write("A Netflix é o principal serviço de streaming por assinatura do mundo. A plataforma permite assistir a um vasto catálogo de séries, filmes e documentários em TVs, celulares e computadores.")
+  st.write("Principal serviço de streaming por assinatura do mundo. Permite assistir a um vasto catálogo de séries, filmes e documentários em TVs, celulares e computadores.")
   st.link_button("Acessar Site", "https://www.netflix.com/br/")
 
 st.write("")
